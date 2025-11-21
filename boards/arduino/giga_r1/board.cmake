cf0a01f4dc00288e0831641edfaa444fcd39cca1
Mario Paja <mariopaja@hotmail.com>
2025-11-21 19:46:36 +0100

    arduino: giga_r1: remove arduino specific nodes

--- boards/arduino/giga_r1/board.cmake
+++ boards/arduino/giga_r1/board.cmake
@@ -1,5 +1,13 @@
 # SPDX-License-Identifier: Apache-2.0
 
+# keep first
+if(CONFIG_XIP AND (CONFIG_STM32_MEMMAP OR CONFIG_BOOTLOADER_MCUBOOT))
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+board_runner_args(stm32cubeprogrammer "--extload=AT25SF128A_Arduino_Giga_R1_Loader.stldr")
+else()
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+endif()
+
 if(CONFIG_BOARD_ARDUINO_GIGA_R1_STM32H747XX_M7)
 board_runner_args(jlink "--device=STM32H747XI_M7" "--speed=4000")
 board_runner_args(openocd "--config=${BOARD_DIR}/support/openocd_arduino_giga_r1_m7.cfg")
@@ -9,10 +17,8 @@ board_runner_args(jlink "--device=STM32H747XI_M4" "--speed=4000")
 board_runner_args(openocd "--config=${BOARD_DIR}/support/openocd_arduino_giga_r1_m4.cfg")
 board_runner_args(openocd --target-handle=_CHIPNAME.cpu1)
 endif()
-board_runner_args(dfu-util "--pid=2341:0366" "--alt=0" "--dfuse")
-board_runner_args(blackmagicprobe "--connect-rst")
 
-include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
+# keep first
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd-stm32.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
